c183027048faf9942eed091b1e00785927dd4f4b
Sheik Abdullah Sheriff H <abdullah.h@mitrahsoft.com>
2025-01-03 11:50:32 +0530

    Updated the basic config

--- fstrToken/ignition/modules/Fstr_Token.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("FstrModule", (m) => {
-  const Fstr_Token = m.contract("Token");
-
-  return { Fstr_Token };
-});
